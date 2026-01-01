02c9fc6a45e4cd2223966e3739eae019bda16e0c
Sergio Matamoros <124097535+SmatamorosN04@users.noreply.github.com>
2026-01-01 12:28:24 -0600

    the tasks can be modified and the change can be saved, and destroy button only appears when the cursor is on it

--- src/index.js
+++ src/index.js
@@ -39,6 +39,51 @@ todoList.addEventListener("change", (event) => {
   li.classList.toggle("completed", todo.completed);
 });
 
+todoList.addEventListener("dblclick", (event) => {
+  if (!event.target.tagName === "LABEL") return;
+  const li = event.target.closest("li");
+  li.classList.add("editing");
+
+  const input = li.querySelector(".edit");
+  input.focus();
+  input.setSelectionRange(0, input.value.length);
+});
+
+todoList.addEventListener("click", (event) => {
+
+  if (!event.target.classList.contains("destroy")) return;
+
+  const li = event.target.closest("li");
+  const todoId = li.dataset.id;
+
+  const index = all.findIndex((t) => t.id === todoId);
+  if (index > -1) all.splice(index, 1);
+
+  renderAll();
+});
+
+todoList.addEventListener("keyup", (event) => {
+  if (!event.target.classList.contains("edit")) return;
+
+  const li = event.target.closest("li");
+  const todoId = li.dataset.id;
+  const todo = all.find((t) => t.id === todoId);
+  if (!todo) return;
+
+  if (event.key === "Enter") {
+    const newTitle = event.target.value.trim();
+    if (newTitle !== "") {
+      todo.title = newTitle;
+      renderAll();
+    }
+    li.classlist.remove("editing");
+  }
+  if (event.key === "Escape") {
+    li.classList.remove("editing");
+    renderAll();
+  }
+});
+
 window.addEventListener("DOMContentLoaded", () => {
   inputTodo.focus();
 });
