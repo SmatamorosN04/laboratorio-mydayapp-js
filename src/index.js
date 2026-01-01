db37249eb4d2b19d63a77cf72fabddb2e99ef39f
Sergio Matamoros <124097535+SmatamorosN04@users.noreply.github.com>
2026-01-01 12:03:12 -0600

    tasks can be added to the todo list and can be checked

--- src/index.js
+++ src/index.js
@@ -22,9 +22,44 @@ inputTodo.addEventListener("keyup", (event) => {
 
     inputTodo.value = "";
     console.log(all);
+
+    renderAll();
   }
+});
+todoList.addEventListener("change", (event) => {
+  if (!event.target.classList.contains("toggle")) return;
+
+  const li = event.target.closest("li");
+  const todoId = li.dataset.id;
+  const todo = all.find((t) => t.id === todoId);
+
+  if (!todo) return;
+
+  todo.completed = !todo.completed;
+  li.classList.toggle("completed", todo.completed);
 });
 
 window.addEventListener("DOMContentLoaded", () => {
   inputTodo.focus();
 });
+
+function renderAll() {
+  todoList.innerHTML = "";
+
+  all.forEach((todo) => {
+    const li = document.createElement("li");
+    li.dataset.id = todo.id;
+
+    if (todo.completed) li.classList.add("completed");
+
+    li.innerHTML = `
+    <div class="view">
+    <input class="toggle" type="checkbox" ${todo.completed ? "checked" : ""}>
+    <label> ${todo.title}</label>
+    <button class="destroy"></button>
+    </div>
+    <input class="edit" value="${todo.title}">
+    `;
+    todoList.appendChild(li);
+  });
+}
